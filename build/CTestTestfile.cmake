5b305be806b0d7d5abfc996e48c63836e43ae4ce
IgararA <promwcx@163.com>
2021-10-23 17:28:47 +0800

    catkin_init_workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/bobac2/bobac2_secondary_development/src
+# Build directory: /home/bobac2/bobac2_secondary_development/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/bobac2/bobac2_secondary_development/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/kinetic/bin:/home/bobac2/bin:/home/bobac2/.local/bin:/home/bobac2/miniconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/bobac2/bobac2_secondary_development/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/bobac2/bobac2_secondary_development/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/bobac2/bobac2_secondary_development/src:$ROS_PACKAGE_PATH"
